8dcf1f4982aa6a460950f1f0d878d61f1587013c
Charlie Vieth <cviethjr@pivotal.io>
2017-08-11 17:58:50 -0400

    added get description

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-data = {"person_id":2854507,"project":{"id":2025095,"version":2549},"command":{"type":"story_update","command_uuid":"020a9ba0-40e1-4dc8-a667-4cb7bf8b320e","parameters":{"owner_ids":[1915255],"id":149819592}}}
+var data = {"person_id":2854507,"project":{"id":2025095,"version":2549},"command":{"type":"story_update","command_uuid":"020a9ba0-40e1-4dc8-a667-4cb7bf8b320e","parameters":{"owner_ids":[1915255],"id":149819592}}}
+
+var data_fromGetStory = {"kind":"story","id":149819592,"created_at":"2017-07-31T19:27:20Z","updated_at":"2017-08-11T21:31:31Z","estimate":2,"story_type":"feature","name":"Services running on Windows cells documented","description":"description","current_state":"unstarted","requested_by_id":1582590,"url":"url","project_id":2025095,"owner_ids":[1915255],"labels":[{"id":18970447,"project_id":2025095,"kind":"label","name":"stemcell security strategy","created_at":"2017-07-28T20:52:58Z","updated_at":"2017-07-28T20:52:58Z"}],"owned_by_id":1915255}
 
 function getProjectId(data) {
 	return data.project.id
@@ -12,16 +14,42 @@ function getStoryOwners(data) {
 	return data.command.parameters.owner_ids
 }
 
+function getStoryUrl(projectId, storyId) {
+	return '/services/v5/projects/'+ projectId + '/stories/' + storyId
+}
+
+function getDescription(data_fromGetStory) {
+	return data_fromGetStory.description
+}
 
 //test framework
+function pass() {
+	document.body.innerHTML += "<br>pass</br>"
+}
+
+function fail(x) {
+	document.body.innerHTML += "<br>fail: "+x+"</br>"
+}
+
 function assertIsNotNull(x) {
 	if(x==null)
-  	document.body.innerHTML += "<br>fail: was null</br>"
+  	fail('was null')
   else
-  	document.body.innerHTML += "<br>pass</br>"
+  	pass()
+}
+
+function assertEqual(a,b) {
+	if(a==b)
+  	pass()
+  else
+  	fail('expected: ' + a + ' but was: ' + b)
 }
 
 //tests
 assertIsNotNull(getProjectId(data))
 assertIsNotNull(getStoryId(data))
 assertIsNotNull(getStoryOwners(data))
+assertEqual('/services/v5/projects/a/stories/b', getStoryUrl('a','b'))
+assertEqual('description', getDescription(data_fromGetStory))
+
+

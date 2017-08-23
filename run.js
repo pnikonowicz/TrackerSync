0d2b888bc30a71fdc5a4376a64f1586e98caf81e
Paul Nikonowicz <pnikonowicz@pivotal.io>
2017-08-23 09:29:40 -0400

    WIP
    Signed-off-by: Natalie Arellano <narellano@pivotal.io>

--- run.js
+++ run.js
@@ -1,22 +1,17 @@
 console.log("starting")
 
-function main_init() {
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('main.js');
 
+s.onload = function() {
+  console.log("loaded")
   var s = document.createElement('script');
-  s.src = chrome.extension.getURL('main.js');
-
+  var code = "console.log('running main'); main()"
+  s.textContent = code
   s.onload = function() {
     console.log("loaded")
-    var s = document.createElement('script');
-    var code = "console.log('running main'); main()"
-    s.textContent = code
-    s.onload = function() {
-      console.log("loaded")
-    };
-    (document.head || document.documentElement).appendChild(s);
   };
   (document.head || document.documentElement).appendChild(s);
-}
+};
+(document.head || document.documentElement).appendChild(s);
 
-main_init()
-popup_init()
